c298fb073d8c1e75b5f7844b1f2749d588b65eb2
merlinsalomirose <merlinrose@intersmart.in>
2025-06-17 16:15:39 +0530

    footer

--- src/components/layout/Footer/index.jsx
+++ src/components/layout/Footer/index.jsx
@@ -11,13 +11,13 @@ const footerHeading = "lg:text-[11px] text-[10px] uppercase text-white/40 font-m
 
 export default function footer() {
   return (
-    <footer className="w-full h-auto block bg-[#07163D] py-[120px_60px]">
+    <footer className="w-full h-auto block bg-[#07163D] 2xl:py-[120px_60px] sm:py-[80px_40px] py-[40px_30px]">
       <div className="container">
         <div className="w-full flex flex-wrap">
-          <div className="w-[25%] pr-[60px]">
+          <div className="xl:w-[25%] sm:w-[50%] w-[100%]  sm:pr-[60px] xl:mb-[0] mb-[20px]">
             <Link
               href="#"
-              className="w-[320px] h-[135px] aspect-square block mb-[50px]"
+              className="3xl:w-[320px] 3xl:h-[135px] 2xl:w-[260px] 2xl:h-[100px] xl:w-[200px] xl:h-[85px] w-[200px] h-[85px] aspect-square block 3xl:mb-[50px] 2xl:mb-[30px] mb-[20px]"
             >
               <Image
                 src="/images/footer-logo.webp"
@@ -27,73 +27,73 @@ export default function footer() {
                 className="w-full h-full block object-contain"
               />
             </Link>
-            <p className="text-[20px] leading-[1.3] font-normal text-white ">
+            <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] text-[14px] leading-[1.3] font-normal text-white ">
               Shayan Royal General Trading was established in 1995 in Dubai, United Arab Emirates and has since extended its core business into exporting of Brand-new cars from Middle East.
             </p>
           </div>
-          <div className="w-[20%] pr-[60px]">
+          <div className="xl:w-[20%] sm:w-[50%] w-[100%] sm:pr-[60px] xl:mb-[0] mb-[20px]">
             <Heading
               as="h6"
-              className="text-[20px] leading-none font-semibold uppercase text-white mb-[30px]"
+              className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] leading-none font-semibold uppercase text-white sm:mb-[30px] mb-[20px]"
             >
               Quick Link
             </Heading>
             <ul>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   About Us
                 </Link>
               </li>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   Inventory
                 </Link>
               </li>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   Brands Specialized In
                 </Link>
               </li>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   Services
                 </Link>
               </li>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   Privacy Policy
                 </Link>
               </li>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   Terms & Condition
                 </Link>
               </li>
-              <li>
-                <Link href="#" className="text-[20px] block text-white mb-[15px] hover:text-[#BE1E2D]" >
+              <li className='mb-[10]'>
+                <Link href="#" className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] text-white mb-[15px] hover:text-[#BE1E2D]" >
                   Contact Us
                 </Link>
               </li>
             </ul>
           </div>
-          <div className="w-[25%] pr-[60px]">
+          <div className="2xl:w-[25%] xl:w-[30%] sm:w-[50%] w-[100%] sm:pr-[60px] ">
             <Heading
               as="h6"
-              className="text-[20px] leading-none font-semibold uppercase text-white mb-[30px]"
+              className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] leading-none font-semibold uppercase text-white sm:mb-[30px] mb-[20px]"
             >
               visit us
             </Heading>
-            <p className="text-[20px] leading-[1.3] font-normal text-white mb-[85px]">
+            <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] text-[14px] leading-[1.3] font-normal text-white 3xl:mb-[85px] 2xl:mb-[60px] xl:mb-[40px] mb-[40px]">
               Centurion Star Tower, behind Day To Day,Port Saeed 34 St. Deira.
               Dubai, United Arab Emirates
             </p>
             <Heading
               as="h6"
-              className="text-[20px] leading-none font-semibold uppercase text-white mb-[10px]"
+              className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] leading-none font-semibold uppercase text-white mb-[10px]"
             >
               Stay in the loop.
             </Heading>
-            <p className="text-[16px] leading-[1.3] font-normal text-white mb-[25px]">
+            <p className="xl:text-[16px] text-[14px] leading-[1.3] font-normal text-white mb-[25px]">
               Sign up for email updates today.
             </p>
             <div className="w-full flex items-center relative z-0">
@@ -104,22 +104,22 @@ export default function footer() {
               />
               <Button
                 type="submit"
-                className="absolute top-0 right-[10px] bottom-0 margin-auto bg-[#BE1E2D] h-[40px] top-1/2 -translate-y-1/2 right-[3px] text-[16px] uppercase font-medium">
+                className="absolute top-0 right-[10px] bottom-0 margin-auto bg-[#BE1E2D] h-[40px] top-1/2 -translate-y-1/2 right-[3px] 3xl:text-[16px] 2xl:text-[14px] text-[14px] uppercase font-medium">
                 Subscribe
               </Button>
             </div>
           </div>
-          <div className="w-[30%] ">
+          <div className="2xl:w-[30%] xl:w-[25%] sm:w-[50%] w-[100%] sm:mt-[0px] mt-[25px]">
             <Heading
               as="h6"
-              className="text-[20px] leading-none font-semibold uppercase text-white mb-[30px]"
+              className="3xl:text-[20px] 2xl:text-[18px] sm:text-[16px] text-[14px] leading-none font-semibold uppercase text-white sm:mb-[30px] mb-[20px]"
             >
               our Location
             </Heading>
             <div>
               <Link
                 href="https://maps.app.goo.gl/z5LV1Zd5tjzemEAZ7"
-                className="w-[490px] h-[283px] aspect-square block mb-[50px] rounded-[10px] overflow-hidden w-full"
+                className="w-full 3xl:h-[283px] 2xl:h-[255px] xl:h-[215px] xl:h-[215px] sm:h-[195px] h-[160px] aspect-square block sm:mb-[50px] mb-[20px] rounded-[10px] overflow-hidden w-full"
               >
                 <Image
                   src="/images/map.webp"
@@ -132,81 +132,81 @@ export default function footer() {
             </div>
           </div>
         </div>
-        <div className="bg-[#0E1D44] pt-[20px] pr-[50px] pb-[20px] pl-[30px] rounded-[10px] mb-[45px]">
-          <div className='flex flex-wrap'>
-            <div className='flex flex-wrap'>
-              <div className='text-[50px] font-semibold text-white'>
+        <div className="bg-[#0E1D44] lg:pt-[20px] lg:pr-[50px] lg:pb-[20px] lg:pl-[30px] pt-[20px] pr-[20px] pb-[20px] pl-[20px] rounded-[10px] sm:mb-[45px] mb-[20px]">
+          <div className='flex flex-wrap items-center'>
+            <div className='flex flex-wrap items-center'>
+              <div className='3xl:text-[50px] 2xl:text-[40px] lg:text-[30px] lg:text-[25px] md:text-[20px] sm:text-[16px] text-[14px] font-semibold text-white sm:mb-[0px] mb-[10px]'>
                 CONNECT WITH US
               </div>
-              <div className='pl-[40px]'>
+              <div className='lg:pl-[40px] md:pl-[30px] pl-[20px] sm:mb-[0px] mb-[10px]'>
                 <Image
                   src="/images/arrow.webp"
                   alt="logo"
                   width="78"
                   height="15"
-                  className="w-full h-full block object-contain"
+                  className=" h-full xl:w-[78px] lg:w-[60px] md:w-[40px] w-[30px] block object-contain"
                 />
               </div>
             </div>
-            <div className='flex flex-wrap'>
-              <div className='pl-[60px]'>
-                <div className='text-[20px] font-medium text-white'>EMAIL</div>
-                <a href="mailto:sales@shayan.ae" className='block text-white text-[20px] hover:text-[#BE1E2D]'>sales@shayan.ae</a>
+            <div className='flex flex-wrap items-center sm:w-auto w-full'>
+              <div className='3xl:pl-[60px] 2xl:pl-[40px] md:pl-[30px] sm:pl-[20px] 2xs:w-auto w-full 2xs:mb-[0px] mb-[10px]'>
+                <div className='3xl:text-[20px] lg:text-[18px] text-[14px] font-medium text-white'>EMAIL</div>
+                <a href="mailto:sales@shayan.ae" className='block text-white 3xl:text-[20px] lg:text-[18px] text-[14px] hover:text-[#BE1E2D]'>sales@shayan.ae</a>
               </div>
-              <div className='pl-[60px]'>
-                <div className='text-[20px] font-medium text-white'>TELEPHONE</div>
-                <a href="tel:+97142728150" className='block text-white text-[20px] hover:text-[#BE1E2D]'>+971 4 272 8150</a>
+              <div className='3xl:pl-[60px] 2xl:pl-[40px] md:pl-[30px] sm:pl-[20px] 2xs:pl-[30px] 2xs:w-auto w-full 2xs:mb-[0px] mb-[10px]'>
+                <div className='3xl:text-[20px] lg:text-[18px] text-[14px] font-medium text-white'>TELEPHONE</div>
+                <a href="tel:+97142728150" className='block text-white 3xl:text-[20px] lg:text-[18px] text-[14px] hover:text-[#BE1E2D]'>+971 4 272 8150</a>
               </div>
-              <div className='pl-[60px]'>
-                <div className='text-[20px] font-medium text-white'>WHATSAPP</div>
-                <a href="https://wa.me/971505286045" target="_blank" className='block text-white text-[20px] hover:text-[#BE1E2D]'>+971 50 528 6045</a>
+              <div className='3xl:pl-[60px] 2xl:pl-[40px] md:pl-[30px] sm:pl-[20px] 2xs:pl-[30px] 2xs:w-auto w-full'>
+                <div className='3xl:text-[20px] lg:text-[18px] text-[14px] font-medium text-white'>WHATSAPP</div>
+                <a href="https://wa.me/971505286045" target="_blank" className='block text-white 3xl:text-[20px] lg:text-[18px] text-[14px] hover:text-[#BE1E2D]'>+971 50 528 6045</a>
               </div>
             </div>
-            <div className='flex flex-wrap pl-[90px]'>
-              <a href="" className='pr-[25px] transition-all duration-500 hover:scale-110'>
+            <div className='flex flex-wrap items-center lg:justify-start sm:justify-center justify-start 2xl:pl-[90px] xl:pl-[50px] xl:mt-[0px] mt-[20px] lg:w-auto w-full xl:mx-0 mx-auto'>
+              <a href="" className='2xl:pr-[25px] xl:pr-[15px] pr-[10px] transition-all duration-500 hover:scale-110'>
                 <Image
                   src="/images/insta.webp"
                   alt="logo"
                   width="46"
                   height="46"
-                  className="w-full h-full block object-contain"
+                  className=" block object-contain 3xl:w-[46px] 3xl:h-[46px] 2xl:w-[40px] 2xl:h-[40px] xl:w-[30px] xl:h-[30px] w-[25px] h-[25px]"
                 />
               </a>
-              <a href="" className='pr-[25px] transition-all duration-500 hover:scale-110'>
+              <a href="" className='2xl:pr-[25px] xl:pr-[15px] pr-[10px] transition-all duration-500 hover:scale-110'>
                 <Image
                   src="/images/fb.webp"
                   alt="logo"
                   width="46"
                   height="46"
-                  className="w-full h-full block object-contain"
+                  className="block object-contain 3xl:w-[46px] 3xl:h-[46px] 2xl:w-[40px] 2xl:h-[40px] xl:w-[30px] xl:h-[30px] w-[25px] h-[25px]"
                 />
               </a>
-              <a href="" className='pr-[25px] transition-all duration-500 hover:scale-110'>
+              <a href="" className='2xl:pr-[25px] xl:pr-[15px] pr-[10px] transition-all duration-500 hover:scale-110'>
                 <Image
                   src="/images/youtube.webp"
                   alt="logo"
                   width="46"
                   height="46"
-                  className="w-full h-full block object-contain"
+                  className=" block object-contain 3xl:w-[46px] 3xl:h-[46px] 2xl:w-[40px] 2xl:h-[40px] xl:w-[30px] xl:h-[30px] w-[25px] h-[25px]"
                 />
               </a>
-              <a href="" className='pr-[25px] transition-all duration-500 hover:scale-110'>
+              <a href="" className='2xl:pr-[25px] xl:pr-[15px] pr-[10px] transition-all duration-500 hover:scale-110'>
                 <Image
                   src="/images/linkdin.webp"
                   alt="logo"
                   width="46"
                   height="46"
-                  className="w-full h-full block object-contain"
+                  className=" block object-contain 3xl:w-[46px] 3xl:h-[46px] 2xl:w-[40px] 2xl:h-[40px] xl:w-[30px] xl:h-[30px] w-[25px] h-[25px]"
                 />
               </a>
             </div>
           </div>
         </div>
         <div className='flex flex-wrap justify-between items-center'>
-          <div className='text-white text-[16px]'>
+          <div className='text-white md:text-[16px] text-[14px] sm:mb-[0px] mb-[10px]'>
             © 2025 Shayan Royal Group. All rights reserved.
           </div>
-          <div className='text-white text-[16px] text-right'>
+          <div className='text-white md:text-[16px] text-[14px] text-right'>
             <div className='flex flex-wrap'>
               Designed By:
               <a href="" className='block pl-[10px]'>
