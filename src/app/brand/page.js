91a0a8e09c75a796e6f7025613e17499e1e1786f
merlinsalomirose <merlinrose@intersmart.in>
2025-06-23 14:18:01 +0530

    brand

--- src/app/brand/page.js
+++ src/app/brand/page.js
@@ -1,4 +1,5 @@
 import InnerBanner from "@/components/common/InnerBanner";
+import { BreadCrumb } from "@/components/common/BreadCrumb";
 import PartnershipSection from "@/components/features/brand/PartnershipSection";
 import PremiumBrandsSection from "@/components/features/brand/PremiumBrandsSection";
 import FormSection from "@/components/features/brand/FormSection";
@@ -7,9 +8,15 @@ export default function Page() {
     return (
         <>
             <InnerBanner title="brands" image="/images/brand-banner.webp" alt="brand-banner" />
-             <PartnershipSection />
-             <PremiumBrandsSection  />
-             <FormSection />
+            <BreadCrumb
+                items={[
+                    { label: "HOME", href: "/" },
+                    { label: "BRANDS", isCurrent: true }
+                ]}
+            />
+            <PartnershipSection />
+            <PremiumBrandsSection />
+            <FormSection />
         </>
     );
 }
