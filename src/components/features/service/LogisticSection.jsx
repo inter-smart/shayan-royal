c08e0fc74127200689d2e2162bac83f450904d9d
abish <abish@intersmart.in>
2025-06-25 17:37:13 +0530

    Minor Changes

---
+++ src/components/features/service/LogisticSection.jsx
@@ -0,0 +1,15 @@
+import EnquiryForm from "../../common/EnquiryForm";
+
+
+export default function LogisticSection () {
+    return (
+        <section className="w-full h-auto block 3xl:py-[130px] 2xl:py-[100px] lg:py-[80px] sm:py-[60px] py-[40px]">
+            <div className="container">
+                <EnquiryForm image="/images/fitment-enquiry.webp"
+                    Formtitle="One Click Away Send Your Enquiry"
+                    Formsubtitle="Seamless Logistic"
+                />
+            </div>
+        </section>
+    );
+}
