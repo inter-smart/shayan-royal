c5e8775fb84f0ee09e6f069ae72348d1bc80c83b
vishnu shaji <vishnushaji@intersmart.in>
2025-07-23 10:29:18 +0530

    merged with dev-merlin

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -16,17 +16,18 @@ export default function ProductCard({ car, variant }) {
 
     const defaultPadding = "px-[10px] 3xl:py-[25px] py-[15px]";
     const paddingClass = variant ? (variants[variant] || defaultPadding) : defaultPadding; 
+    const maxheight = variant ? (variants[variant] || defaultPadding) : defaultPadding; 
 
     return (
         <Link href={car.link || "#"}
             aria-label="news" className={`w-full h-full flex cursor-pointer group ${paddingClass}`}>
             <div className="w-full h-full rounded-[10px] bg-white overflow-hidden 3xl:py-[15px] py-[10px] 3xl:px-[20px] px-[15px] flex items-end shadow-xl">
                 {/* Left Section */}
-                <div className="3xl:w-[calc(100%-65px)] w-[calc(100%-45px)] h-full min-h-[200px] 3xs:min-h-[230px] 3xl:min-h-[295px] mr-[20px] 
+                <div className={`3xl:w-[calc(100%-65px)] w-[calc(100%-45px)] h-full min-h-[200px] 3xs:min-h-[230px] 3xl:min-h-[295px] mr-[20px] 
                     relative flex flex-col rounded-[10px] overflow-hidden after:absolute 
                     after:top-0 after:right-0 after:content-[''] 
                     after:bg-[linear-gradient(90deg,_rgba(187,192,207,0.00)_0%,_#BBC0CF_100%)] after:w-full after:max-w-[150px] 
-                    after:opacity-[0.22] after:h-full">
+                    after:opacity-[0.22] after:h-full ${variant === "inventory" ? "after:!h-[70%] after:rounded-[10px] " : ""}`}>
                     {/* Logo */}
                     <div className="w-full 3xl:max-w-[50px] max-w-[40px] absolute top-0 left-0 z-10">
                         <Image
@@ -56,13 +57,13 @@ export default function ProductCard({ car, variant }) {
                 </div>
 
                 {/* Right Section - Specs */}
-                <div className="3xl:w-[65px] w-[45px]">
-                    <div className="flex flex-col 3xl:max-h-[265px] max-h-[200px] h-full">
+                <div className={`3xl:w-[65px] w-[45px] ${variant === "inventory" ? "h-full pb-5" : " "}`}>
+                   <div className={`flex flex-col h-full ${variant === "inventory" ? "max-h-full" : "3xl:max-h-[265px] max-h-[200px]"}`}>
                         {car.specs.map((spec, i) => {
                             const icon = specIcons.find((s) => s.label === spec)?.icon;
 
                             return (
-                                <div key={i} className="3xl:mb-[15px] mb-[10px] last:mb-0">
+                                <div key={i} className="3xl:mb-[15px] mb-[10px] last:mb-0 flex-grow-1">
                                     <div className="text-center w-full h-full rounded-[10px] overflow-hidden bg-[#F5F9FF] 3xl:min-h-[55px] min-h-[40px] flex items-center justify-center flex-col">
                                         <div>
                                             <div className="3xl:w-[22px] w-[15px] 3xl:h-[22px] h-[15px] m-auto mb-[3px] flex">
