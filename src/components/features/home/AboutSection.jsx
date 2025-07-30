74e3e08ffd7010464f0ff08424946571409cccaf
vishnu shaji <vishnushaji@intersmart.in>
2025-07-30 10:59:39 +0530

    bug list

--- src/components/features/home/AboutSection.jsx
+++ src/components/features/home/AboutSection.jsx
@@ -16,7 +16,7 @@ export default function AboutSection() {
                 loading="lazy"
                 className="w-full h-full object-cover absolute top-0 left-0"
             >
-                <source src="/videos/about.mp4" type="video/mp4" />
+                <source src="/videos/social1.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <div className="container">
