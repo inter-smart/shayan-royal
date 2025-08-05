918b2410c5f222c6455a0e358f76d589c4095024
afsalmern <afsal@intersmart.in>
2025-08-05 14:06:37 +0530

    added captchca in forms

--- src/components/features/brand/EnquiryFormSection.jsx
+++ src/components/features/brand/EnquiryFormSection.jsx
@@ -1,12 +1,23 @@
 "use client";
 import { mediaUrl } from "@/lib/constants";
 import EnquiryForm from "../../common/EnquiryForm";
+import RecaptchaProvider from "@/components/layout/RecaptchaProvider";
 
 export default function EnquiryFormSection({ image }) {
   return (
     <section className="w-full h-auto lg:pt-[50px] pt-[30px] lg:pb-[140px] md:pb-[80px] pb-[40px]">
       <div className="container mx-auto px-4">
-        <EnquiryForm image={image ? `${mediaUrl}${image}` : "/images/brand-form.webp"} Formtitle="Enquiry Form" />
+        <RecaptchaProvider
+          reCaptchaKey={siteKey}
+          scriptProps={{
+            async: false,
+            defer: false,
+            appendTo: "head",
+            nonce: undefined,
+          }}
+        >
+          <EnquiryForm image={image ? `${mediaUrl}${image}` : "/images/brand-form.webp"} Formtitle="Enquiry Form" type="brand" />
+        </RecaptchaProvider>
       </div>
     </section>
   );
