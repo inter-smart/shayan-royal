8d863a1b6360c7597253ce1652b56277763b5926
afsalmern <afsal@intersmart.in>
2025-08-05 15:00:48 +0530

    bug fix

--- src/components/features/brand/EnquiryFormSection.jsx
+++ src/components/features/brand/EnquiryFormSection.jsx
@@ -7,15 +7,7 @@ export default function EnquiryFormSection({ image }) {
   return (
     <section className="w-full h-auto lg:pt-[50px] pt-[30px] lg:pb-[140px] md:pb-[80px] pb-[40px]">
       <div className="container mx-auto px-4">
-        <RecaptchaProvider
-          reCaptchaKey={siteKey}
-          scriptProps={{
-            async: false,
-            defer: false,
-            appendTo: "head",
-            nonce: undefined,
-          }}
-        >
+        <RecaptchaProvider>
           <EnquiryForm image={image ? `${mediaUrl}${image}` : "/images/brand-form.webp"} Formtitle="Enquiry Form" type="brands" />
         </RecaptchaProvider>
       </div>
