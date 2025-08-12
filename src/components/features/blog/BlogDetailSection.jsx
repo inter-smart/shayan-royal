94d90b2cd0c6546935b53cd87ecd8d089908c212
afsalmern <afsal@intersmart.in>
2025-08-12 10:53:11 +0530

    Modified the blog comments secction

--- src/components/features/blog/BlogDetailSection.jsx
+++ src/components/features/blog/BlogDetailSection.jsx
@@ -12,6 +12,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import RecaptchaProvider from "@/components/layout/RecaptchaProvider";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import BlogComments from "./BlogComments";
 dayjs.extend(relativeTime);
 
 const items = [
@@ -32,85 +33,6 @@ const items = [
 ];
 
 export default function BlogDetailSection({ blog, recentBlogs, slug }) {
-  const [name, setName] = useState("");
-  const [comment, setComment] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [fetchedComments, setFetchedComments] = useState([]);
-  const [fetching, setFetching] = useState(false);
-
-  const { executeRecaptcha } = useGoogleReCaptcha();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!name.trim()) {
-      toast.warning("Please enter name.");
-      return;
-    }
-
-    if (!comment.trim()) {
-      toast.warning("Please enter commect.");
-      return;
-    }
-
-    const recaptchaToken = await executeRecaptcha("comments");
-    if (!recaptchaToken) {
-      toast.error("Failed to get reCAPTCHA token. Please try again.");
-      setLoading(false);
-      return;
-    }
-
-    setLoading(true);
-    try {
-      const response = await fetch(`${mediaUrl}/api/comments`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ name, content: comment, status: "active", slug, recaptchaToken }),
-      });
-
-      const result = await response.json();
-
-      if (response.ok && result.success) {
-        console.log("RES", result);
-        toast.success(result?.message || "Your comment has been submitted!");
-        setName("");
-        setComment("");
-      } else {
-        toast.error(result.message || "Submission failed.");
-      }
-    } catch (error) {
-      console.error("Comment submission error:", error);
-      toast.error("Something went wrong. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (!slug) return;
-    const fetchComments = async () => {
-      setFetching(true);
-      try {
-        const res = await fetch(`${mediaUrl}/api/comments/get-comments?slug=${slug}`);
-        const data = await res.json();
-        if (data.success) {
-          setFetchedComments(Array.isArray(data.data) ? data.data : []);
-        } else {
-          toast.error(data.message || "Failed to load comments.");
-        }
-      } catch (error) {
-        console.error("Error fetching comments:", error);
-        toast.error("Something went wrong while loading comments.");
-      } finally {
-        setFetching(false);
-      }
-    };
-
-    fetchComments();
-  }, [slug]);
-
   return (
     <section className="w-full h-auto 2xl:py-[50px_130px] lg:py-[30px_85px] sm:py-[30px_50px] py-[20px_40px] block">
       <div className="container">
@@ -152,94 +74,9 @@ export default function BlogDetailSection({ blog, recentBlogs, slug }) {
             </div>
 
             {/* Comment Form */}
-            <div className="flex flex-wrap mt-10 2xl:pt-[65px] xl:pt-[45px] pt-[25px] 2xl:pb-[50px] pb-[30px] border-t border-b border-[#D0D0D0]">
-              <div className="w-full md:w-[45%] xl:w-[37%] 3xl:pr-[55px] xl:pr-[45px] pr-[25px] mb-6 md:mb-0">
-                <h3 className="3xl:text-[40px] 2xl:text-[36px] xl:text-[30px] text-[26px] font-medium uppercase text-[#000] mb-[20px] leading-[1.1]">
-                  Leave a <span className="block"> Reply </span>
-                </h3>
-                <p className="3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] text-[14px] text-[#262626] mb-[40px]">
-                  Share your feedback, questions, or insights in the comments below.
-                </p>
-                {/* Comment Count with Conditional Icon */}
-                <div className="flex items-center">
-                  {fetchedComments.length > 0 && (
-                    <div className="w-[17px] h-[17px] mr-2">
-                      <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M1.57617 4.25237C1.57617 3.17656 2.44829 2.30444 3.52409 2.30444H13.4749C14.5507 2.30444 15.4228 3.17656 15.4228 4.25236V11.3408C15.4228 12.4166 14.5507 13.2887 13.4749 13.2887H5.11105L2.42436 15.2859C2.2632 15.4057 2.04825 15.4244 1.86883 15.3342C1.68941 15.2441 1.57617 15.0604 1.57617 14.8596V4.25237ZM3.52409 3.36694C3.03509 3.36694 2.63867 3.76336 2.63867 4.25237V13.8027L4.61829 12.3311C4.70992 12.263 4.82105 12.2262 4.93522 12.2262H13.4749C13.9639 12.2262 14.3603 11.8298 14.3603 11.3408V4.25236C14.3603 3.76336 13.9639 3.36694 13.4749 3.36694H3.52409Z"
-                          fill="black"
-                        />
-                      </svg>
-                    </div>
-                  )}
-                  <div className="flex items-center">
-                    <span className="3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] text-[14px] text-[#262626] pr-[2px]">
-                      {fetchedComments.length}
-                    </span>
-                    <div className="3xl:text-[20px] 2xl:text-[18px] xl:text-[16px] text-[14px] text-[#262626]">Comments</div>
-                  </div>
-                </div>
-              </div>
-
-              <RecaptchaProvider>
-                <div className="w-full md:w-[55%] xl:w-[63%]">
-                  <form onSubmit={handleSubmit} className="w-full flex flex-col gap-4">
-                    <input
-                      type="text"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      placeholder="Name*"
-                      required
-                      className="w-full bg-[#eeeeee] rounded-md px-4 py-3 text-sm outline-none 2xl:h-[60px] h-[50px] 2xl:placeholder:text-[16px] placeholder:text-[14px]"
-                    />
-                    <textarea
-                      value={comment}
-                      onChange={(e) => setComment(e.target.value)}
-                      placeholder="Comment"
-                      rows={4}
-                      required
-                      className="w-full bg-[#eeeeee] rounded-md px-4 py-3 text-sm outline-none resize-none 2xl:h-[120px] h-[100px] 2xl:placeholder:text-[16px] placeholder:text-[14px]"
-                    />
-                    <div className="flex justify-end">
-                      <button
-                        type="submit"
-                        className="bg-[#2E4C99] xl:mt-[25px] mt-[15px] 3xl:text-[16px] 2xl:text-[14px] text-[12px] text-white text-xs font-medium px-5 py-2 rounded-[50px] hover:bg-[#1d397e] transition xl:w-[178px] w-[165px] xl:h-[40px] h-[35px] flex items-center justify-center"
-                      >
-                        POST COMMENT
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </RecaptchaProvider>
-            </div>
-
-            {/* Comment List */}
-            {fetchedComments.map((comment, idx) => (
-              <div key={idx} className="pt-[15px] xl:pt-[25px] 2xl:pt-[35px] pb-[20px] xl:pb-[30px] 2xl:pb-[40px] border-b border-[#D0D0D0] flex">
-                {/* <div className="w-[22%] 3xs:w-[20%] 2xs:w-[15%] sm:w-[12%] md:w-[10%] lg:w-[12%] xl:w-[10%] 3xl:w-[6%]">
-                  <div className="w-[65px] h-[65px] rounded-full overflow-hidden block">
-                    <Image
-                      src={comment.image}
-                      alt={comment.name}
-                      width={65}
-                      height={65}
-                      className="object-cover w-full h-full"
-                    />
-                  </div>
-                </div> */}
-                <div className="w-full">
-                  <h6 className="text-[16px] md:text-[18px] 3xl:text-[20px] font-medium text-[#262626] mb-[2px] xl:mb-[5px]">
-                    {comment.name}{" "}
-                    <span className="text-[#7E7E7E] text-[11px] md:text-[12px] 3xl:text-[14px] font-normal">
-                      {dayjs(comment.created_at).fromNow()}
-                    </span>
-                  </h6>
-                  <p className="text-[12px] md:text-[14px] xl:text-[16px] 2xl:text-[18px] 3xl:text-[20px] text-[#262626] mt-2">{comment.content}</p>
-                </div>
-              </div>
-            ))}
+            <RecaptchaProvider>
+              <BlogComments slug={slug} />
+            </RecaptchaProvider>
           </div>
 
           {/* Related Blogs Section */}
