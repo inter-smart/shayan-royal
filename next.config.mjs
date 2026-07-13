5038060efc02efa8d72d2be9fca8bae4261421c8
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2026-07-13 20:36:44 +0530

    Set output option to standalone in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     formats: ["image/webp", "image/avif"],
     minimumCacheTTL: 31536000, // 1 year
